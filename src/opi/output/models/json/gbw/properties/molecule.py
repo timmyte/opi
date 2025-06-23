43dac87f06b31b64ff1b578c9cbce2f6f2810ddc
Hagen Neugebauer <38649381+haneug@users.noreply.github.com>
2025-06-23 14:27:38 +0200

    Add Pydantic output models for integrals (S, H, F,  J, and K) (#7)
    * Add Pydantic output models for Integrals (S, H, F, J, and K) and update the notebook on Fock diagonalization

--- src/opi/output/models/json/gbw/properties/molecule.py
+++ src/opi/output/models/json/gbw/properties/molecule.py
@@ -1,4 +1,4 @@
-from pydantic import Field, StrictInt, StrictStr
+from pydantic import Field, StrictFloat, StrictInt, StrictStr
 
 from opi.output.models.base.get_item import GetItem
 from opi.output.models.json.gbw.properties.atom import Atoms
@@ -21,9 +21,23 @@ class Molecule(GetItem):
     molecularorbital: MolecularOrbital
         Contains information about the molecular orbitals
     multiplicity: StrictInt
-        multiplicity of the atom
+        multiplicity of the molecule
+    charge: StrictInt
+        charge of the molecule
     hftyp: StrictSrt
-        used type of HF in the calculation
+        Used shell-type (e.g., UHF/RHF) in the calculation
+    origin: tuple[StrictFloat, StrictFloat, StrictFloat]
+        Origin of the molecule
+    s_matrix: list[list[StrictFloat]]
+        Overlap matrix
+    h_matrix: list[list[StrictFloat]]
+        Hcore matrix (1-el integrals)
+    f_matrix: f_matrix: list[list[list[StrictFloat]]]
+        Fock matrix/matrices
+    j_matrix: list[list[list[StrictFloat]]]
+        Coulomb integrals (2-el integrals)
+    k_matrix: list[list[list[StrictFloat]]]
+        Exchange integrals (2-el integrals)
     pointgroup: StrictStr
         Pointgroup of the molecule
     td_dft: TdDft | None default = None
@@ -37,6 +51,12 @@ class Molecule(GetItem):
     multiplicity: StrictInt | None = None
     charge: StrictInt | None = None
     hftyp: StrictStr | None = None
+    origin: tuple[StrictFloat, StrictFloat, StrictFloat]
+    s_matrix: list[list[StrictFloat]] | None = Field(default=None, alias="s-matrix")
+    h_matrix: list[list[StrictFloat]] | None = Field(default=None, alias="h-matrix")
+    f_matrix: list[list[list[StrictFloat]]] | None = Field(default=None, alias="f-matrix")
+    j_matrix: list[list[list[StrictFloat]]] | None = Field(default=None, alias="j-matrix")
+    k_matrix: list[list[list[StrictFloat]]] | None = Field(default=None, alias="k-matrix")
     pointgroup: StrictStr | None = None
     td_dft: list[TdDft] | None = Field(None, alias="td-dft")
 
