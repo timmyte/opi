77f87d58cb90f0b42f263b2a07238a68dcca348d
Tim Tetenberg <123412573+timmyte@users.noreply.github.com>
2025-09-02 13:58:13 +0200

    Fixed `opi.__version__` which would always display '0.0.0' (#122)
    The metadata for OPI are stored under its PyPI name: `orca-pi`

--- src/opi/__init__.py
+++ src/opi/__init__.py
@@ -1,4 +1,5 @@
 import importlib.metadata
+from warnings import warn
 
 from opi.utils.orca_version import OrcaVersion
 
@@ -6,8 +7,9 @@ __all__ = ["ORCA_MINIMAL_VERSION"]
 
 # // OPI VERSION
 try:
-    __version__ = importlib.metadata.version(__name__)
-except importlib.metadata.PackageNotFoundError:
+    __version__ = importlib.metadata.version("orca-pi")
+except importlib.metadata.PackageNotFoundError as err:
+    warn(str(err), RuntimeWarning)
     __version__ = "0.0.0"  # Fallback for development mode
 
 # // ORCA VERSION
