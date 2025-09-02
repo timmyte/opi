timmyte/opi

commit 94840b783d19ec21f75d2262d7d95998a76f9ef8

Docu: Implemented automatic version detection; Using it for link injection. (#115)
Sphinx now tries to automatically determine the current OPI version from the next best annotated git tag.
If the git tag has the following form "v1.0.0-*" we assume to be on main directly or some feature branch off main.

docs/conf.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import sys
+from subprocess import check_output, CalledProcessError, TimeoutExpired
+from pathlib import Path
+import traceback
 
 from docutils import nodes
 from sphinx.transforms import SphinxTransform
+from semantic_version import Version
 
 # needs_sphinx = '1.0'
 
@@ -63,10 +68,35 @@ project = "ORCA Python Interface Documentation"
 copyright = "2025, FACCTs"
 author = "FACCTs"
 
+
+origin = Path(__file__).resolve().parent
+full_version = ""
 # > The short X.Y version.
-version = "1.0"
-# > The full version, including alpha/beta/rc tags.
-release = "1.0"
+version_short = ""
+# > Using "main" as fallback
+release_branch = "main"
+try:
+    output = check_output(
+        ["git", "--no-advice", "--no-pager", "describe", "--always"],
+        timeout=5,
+        cwd=origin,
+        text=True,
+    )
+except (CalledProcessError, TimeoutExpired, OSError):
+    traceback.print_exc()
+    sys.exit(1)
+else:
+    try:
+        full_version = Version(version_string=output.strip().removeprefix("v"))
+        # > The short X.Y version.
+        version_short = f"{full_version.major}.{full_version.minor}"
+        # > If version is '1.0.0-*' assume we are on "main" branch or some feature branch based off "main" directly
+        if not (full_version.major == 1 and full_version.minor == 0 and full_version.patch == 0):
+            release_branch = f"release/{version_short}"
+    except:
+        traceback.print_exc()
+        sys.exit(1)
+
 
 # > Language
 language = "English"
@@ -98,10 +128,6 @@ suppress_warnings = [
 ]
 
 # > -- Options for HTML output ----------------------------------------------
-
-html_title = f"OPI {version} Docs"
-html_logo = "img/assets/opi_logo_thumbnail.svg"
-
 # > The theme to use for HTML and HTML Help pages.  See the documentation for
 # > a list of builtin themes.
 html_theme = "furo"
@@ -109,7 +135,7 @@ html_theme = "furo"
 # > Theme options are theme-specific and customize the look and feel of a theme
 # > further.  For a list of options available for each theme, see the
 # > documentation.
-html_title = f"OPI {version} Docs"
+html_title = f"OPI {version_short} Docs"
 html_logo = "img/assets/opi_logo_thumbnail.svg"
 
 html_show_sphinx = False
@@ -208,7 +234,7 @@ class InjectNotebookButtons(SphinxTransform):
         html = ""
         if docname.startswith("contents/notebooks/"):
             ipynb_file = f"_static/{docname}.ipynb"
-            github_url = f"https://github.com/faccts/opi/blob/main/docs/{docname}.ipynb"
+            github_url = f"https://github.com/faccts/opi/blob/{release_branch}/docs/{docname}.ipynb"
             html = f"""
             <div class="button-row">
                 <a href="{github_url}" class="orca-btn">View on GitHub <i class="fab fa-github"></i></a>

commit 1e480db1909eb6c1d1dab2bf30afcf6837e86970

Docu: "--no-advice" would crash with older git versions. (#125)
Fixes bug introduced with PR https://github.com/faccts/opi/pull/115
Replaced `--no-advice` with env. variable: `$GIT_ADVICE=0`

docs/conf.py
@@ -75,9 +75,11 @@ full_version = ""
 version_short = ""
 # > Using "main" as fallback
 release_branch = "main"
+# > Disabling git advices
+os.environ["GIT_ADVICE"] = "0"
 try:
     output = check_output(
-        ["git", "--no-advice", "--no-pager", "describe", "--always"],
+        ["git", "--no-pager", "describe", "--always"],
         timeout=5,
         cwd=origin,
         text=True,

src/opi/utils/orca_version.py
@@ -12,14 +12,14 @@ class OrcaVersion(Version):  # type: ignore
 
     RGX_VERSION = re.compile(
         # major version
-        r"(?P<major>\d+)"  
+        r"(?P<major>\d+)"
         # minor version
-        r"\.(?P<minor>\d+)"  
+        r"\.(?P<minor>\d+)"
         r"("
         # micro version [optional]
         r"\.(?P<micro>\d+)"
         # bugfix version [optional]
-        r"(-(?P<bugfix>f\.\d+))?"  
+        r"(-(?P<bugfix>f\.\d+))?"
         r")?",
         re.VERBOSE | re.IGNORECASE,
     )